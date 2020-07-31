a234d11b3f107c2a1ff2c2d1732edfd0ce7c0b9e
Christian Mercado-Astarita <64434462+chrisleo34@users.noreply.github.com>
2020-07-31 00:11:36 -0700

    Add files via upload

---
+++ PropertyRental/Controllers/CatalogController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using PropertyRental.Models;
+
+namespace PropertyRental.Controllers
+{
+
+    namespace PropertyRental.Controllers
+    {
+
+        public class CatalogController : Controller
+        {
+
+            public IActionResult Test()
+            {
+                return View();
+            }
+
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            public IActionResult Register()
+            {
+                return View();
+            }
+        }
+    }
+}
